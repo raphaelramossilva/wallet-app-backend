fea224828313b4ae9781b7c1774686f2a224ae8f
Raphael Ramos <raphaelramossilva@icloud.com>
2023-07-09 20:45:08 -0300

    connect database to api

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const db = require("./db");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Olá, essa é a aplicação Wallet App!");
+});
+
+app.get("/categories", (req, res) => {
+  db.query("SELECT * FROM categories", (error, response) => {
+    if (error) {
+      return res.status(500).json(error);
+    }
+
+    return res.status(200).json(response.rows);
+  });
+});
+
+app.listen(port, () => {
+  db.connect()
+    .then(() => {
+      console.log("DB connected");
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+  console.log(`Example app listening on port ${port}`);
+});
