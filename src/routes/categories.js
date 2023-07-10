0fd471e7ebe1d93b4eea22228756033c8a0adaf6
Raphael Ramos <raphaelramossilva@icloud.com>
2023-07-10 11:24:09 -0300

    feat: put method for categories

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -12,12 +12,15 @@ const findOne = (id) => {
 
 router.get("/", (req, res) => {
   try {
-    db.query("SELECT * FROM categories", (error, response) => {
-      if (error) {
-        return res.status(500).json(error);
+    db.query(
+      "SELECT * FROM categories ORDER BY name ASC",
+      (error, response) => {
+        if (error) {
+          return res.status(500).json(error);
+        }
+        return res.status(200).json(response.rows);
       }
-      return res.status(200).json(response.rows);
-    });
+    );
   } catch (error) {
     return res.status(500).json(error);
   }
@@ -63,10 +66,44 @@ router.delete("/:id", async (req, res) => {
     const text = "DELETE FROM categories WHERE id=$1 RETURNING *";
     const values = [Number(id)];
     const deleteResponse = await db.query(text, values);
-    if (!category.rows[0]) {
+    if (!deleteResponse.rows[0]) {
       return res.status(400).json({ error: "Category not deleted" });
     }
-    return res.status(200).json(deleteResponse.rows);
+    return res.status(200).json(deleteResponse.rows[0]);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    if (!id) {
+      return res.status(400).json({ error: "Param id is mandatory." });
+    }
+
+    if (name.length < 3) {
+      return res
+        .status(400)
+        .json({ error: "Name should have more than 3 characteres" });
+    }
+
+    const query = findOne(id);
+    const category = await db.query(query);
+    if (!category.rows[0]) {
+      return res.status(404).json({ error: "Category not found" });
+    }
+
+    const text = "UPDATE categories SET name=$1 WHERE id=$2 RETURNING *";
+    const values = [name, Number(id)];
+
+    const updateResponse = await db.query(text, values);
+    if (!updateResponse.rows[0]) {
+      return res.status(400).json({ error: "Category not updated" });
+    }
+    return res.status(200).json(updateResponse.rows[0]);
   } catch (error) {
     return res.status(500).json(error);
   }
